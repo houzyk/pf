b118d3990e830291e1f1643801034e9521616342
Houzair Koussa <houzairmk@icloud.com>
2022-11-10 21:17:01 +0400

    [dev] ADD outline theme

--- src/themes.ts
+++ src/themes.ts
@@ -1,14 +1,17 @@
 export interface ThemeInterface {
   text: string;
+  outline: string;
   background: string;
 }
 
 export const lightTheme: ThemeInterface = {
-  text: '#000000',
+  text: "#000000",
+  outline: "#000000",
   background: "#FFFFFF"
 }
 
 export const darkTheme: ThemeInterface = {
   text: "#FFFFFF",
+  outline: "#FFFFFF",
   background: "#000000"
 }
