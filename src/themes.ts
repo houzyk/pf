86c24555ea0f2ceb605136c79b4d780e794986ca
Houzair Koussa <houzairmk@icloud.com>
2022-11-10 19:51:04 +0400

    [dev] ADD themse object

---
+++ src/themes.ts
@@ -0,0 +1,14 @@
+export interface ThemeInterface {
+  text: string;
+  background: string;
+}
+
+export const lightTheme: ThemeInterface = {
+  text: '#000000',
+  background: "#FFFFFF"
+}
+
+export const darkTheme: ThemeInterface = {
+  text: "#FFFFFF",
+  background: "#000000"
+}
