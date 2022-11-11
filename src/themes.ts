6baa7af8966c72f1172d142f93b1627a2e75fa1e
Houzair Koussa <houzairmk@icloud.com>
2022-11-12 00:11:35 +0400

    [dev] ADD progress bar theme

--- src/themes.ts
+++ src/themes.ts
@@ -2,18 +2,21 @@ interface ThemeInterface {
   text: string;
   outline: string;
   background: string;
+  progressBar: string;
 }
 
 const lightTheme: ThemeInterface = {
   text: "#000000",
   outline: "#000000",
-  background: "#FFFFFF"
+  background: "#FFFFFF",
+  progressBar: "#ed33b9"
 }
 
 const darkTheme: ThemeInterface = {
   text: "#FFFFFF",
   outline: "#FFFFFF",
-  background: "#000000"
+  background: "#000000",
+  progressBar: "#FFFFFF"
 }
 
 export type { ThemeInterface };
