fd0f65f6956ef60a4a17a9d8f511ffff8f9ef68c
Houzair Koussa <houzairmk@icloud.com>
2022-11-11 22:37:26 +0400

    [dev] ADD ismobile context

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,26 @@ import { FooterData,
   ThemeToggleData 
 } from "./data";
 
+const IsMobileContext = React.createContext<boolean>(true);
+
 const App = () => {
 
   const [isLightGlobalTheme, setIsLightGlobalTheme] = useState<boolean>(true);
+  const [isMobile, setIsMobile] = useState<boolean>(true);
+  
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 375);
+
+    window.addEventListener("resize", OnWindowResize);
+
+    return (() => {
+      window.removeEventListener("resize", OnWindowResize);
+    });
+  }, []);
+
+  const OnWindowResize = () => {
+    setIsMobile(window.innerWidth <= 375);
+  }
 
   return (
     <ThemeProvider theme={isLightGlobalTheme ? lightTheme : darkTheme}>
