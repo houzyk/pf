1bb0a7270c5ce8041ecf40a8e88db1d92c3c4d48
Houzair Koussa <houzairmk@icloud.com>
2022-11-11 22:37:07 +0400

    [dev] ADD nav data

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,7 +1,8 @@
 import { NavPropsInterface } from "../components";
 
 const navData: NavPropsInterface = {
-
+  navTitle: "Houzair Koussa.",
+  navTitleMobile: "Houzair."
 }
 
 export default navData;
