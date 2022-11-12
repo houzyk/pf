bdbe448b44247699348f03cf4a632802e6ecc5e5
Houzair Koussa <houzairmk@icloud.com>
2022-11-12 19:47:46 +0400

    [dev] ADD work section mock data

---
+++ src/data/work-section.ts
@@ -0,0 +1,44 @@
+import { WorkSectionsPropsInterface } from "../components";
+
+const workSectionData: WorkSectionsPropsInterface = {
+  accordionsData: [
+    {
+      accordionTitle: "Pampers",
+      accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTechParagraph: "React Next Typescript GQL Azure JavaScript",
+      accordionTheme: "#00A3A3"
+    },
+    {
+      accordionTitle: "Rapp",
+      accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTechParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTheme: "#fd7e14"
+    },
+    {
+      accordionTitle: "Mercedes Benz",
+      accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTechParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTheme: "#999" 
+    },
+    {
+      accordionTitle: "Oceana Labs",
+      accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTechParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTheme: "#d63384"
+    }, 
+    {
+      accordionTitle: "Le Bateau",
+      accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTechParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTheme: "#62cacB"
+    },
+    {
+      accordionTitle: "My Posh",
+      accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTechParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
+      accordionTheme: "#FF5964"
+    }
+  ]
+}
+
+export default workSectionData;
