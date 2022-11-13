bf265d1a763ed34c0cca5642f07feed3d0161d71
Houzair Koussa <houzairmk@icloud.com>
2022-11-13 22:05:55 +0400

    [dev] UPDATE data

--- src/data/work-section.ts
+++ src/data/work-section.ts
@@ -6,7 +6,7 @@ const workSectionData: WorkSectionsPropsInterface = {
     {
       accordionTitle: "Pampers",
       accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
-      accordionTechParagraph: ["ReactJS", "NextJS", "TypeScript", "Azure DevOps", "GraphQL", "Styled-Components", "Jest", "Enzyme", "Apollo", "Contentful", "ES6", "NodeJS", "Git", "Storybook", "Lerna", "NPM", "Webpack", "SonarQube", "ShellJS", "SCSS"],
+      accordionTechParagraph: ["ReactJS", "NextJS", "TypeScript", "Azure DevOps", "GraphQL", "Styled-Components", "Jest", "Enzyme", "Apollo", "Contentful", "ES6", "NodeJS", "Git", "Storybook", "Lerna", "NPM", "Webpack", "SonarQube", "SCSS"],
       accordionTheme: "#00A3A3",
       accordionButtonText: "GO TO PAMPERS USA",
       accordionButtonURL: "https://www.pampers.com/en-us"
@@ -14,7 +14,7 @@ const workSectionData: WorkSectionsPropsInterface = {
     {
       accordionTitle: "Rapp",
       accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
-      accordionTechParagraph: ["GSAP", "ES6", "PUG", "jQuery", "CSS", "HTML5", "Github Actions"],
+      accordionTechParagraph: ["GSAP", "ES6", "PUG", "jQuery", "CSS", "HTML5"],
       accordionTheme: "#fd7e14",
       accordionButtonText: "VISIT RAPP.COM",
       accordionButtonURL: "https://rapp.com/"
@@ -22,13 +22,13 @@ const workSectionData: WorkSectionsPropsInterface = {
     {
       accordionTitle: "Mercedes Benz UK",
       accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
-      accordionTechParagraph: ["VueJS", "ES6", "ThreeJS", "GSAP"],
+      accordionTechParagraph: ["VueJS", "ES6", "ThreeJS", "GSAP", "NGROK", "Github Actions"],
       accordionTheme: "#999"
     },
     {
       accordionTitle: "Oceana Labs",
       accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
-      accordionTechParagraph: ["ReactJS", "Solidity", "IPFS", "Storybook", "Fleek", "web3-react", "Etheureum Blockchain", "Metamask", "Truffle", "Hard Hat", "ES6", "NodeJS"],
+      accordionTechParagraph: ["ReactJS", "Solidity", "IPFS", "Storybook", "Fleek", "web3-react", "Etheureum Blockchain", "Metamask", "Truffle", "Hard Hat", "ES6", "NodeJS", "Ethers"],
       accordionTheme: "#d63384",
       accordionButtonText: "EXPLORE OCEANA LABS",
       accordionButtonURL: "https://oceana-lab.com/"
@@ -36,7 +36,7 @@ const workSectionData: WorkSectionsPropsInterface = {
     {
       accordionTitle: "Le Bateau",
       accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
-      accordionTechParagraph: ["Ruby", "Ruby On Rails", "StimulusJS", "Reddis", "Postgres", "Twillio API", "SCSS"],
+      accordionTechParagraph: ["Ruby", "Ruby On Rails", "StimulusJS", "Reddis", "Postgres", "Twillio API", "SCSS", "Figma", "Heroku", "SQL", "Git", "Devise", "Faker", "Cloudinary", "Rails Pundit", "Web Socket", "Bootstrap"],
       accordionTheme: "#62cacB",
       accordionButtonText: "VISIT LE BATEAU",
       accordionButtonURL: "https://lebateau712.herokuapp.com"
@@ -44,7 +44,7 @@ const workSectionData: WorkSectionsPropsInterface = {
     {
       accordionTitle: "My Posh",
       accordionDescriptionParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta suscipit voluptate nemo iusto pariatur eos, doloribus porro? Necessitatibus recusandae autem dicta laborum laudantium delectus et error praesentium, ab perspiciatis?",
-      accordionTechParagraph: ["Ruby", "Ruby On Rails", "MapBox API", "Elastic Search", "Postgres"],
+      accordionTechParagraph: ["Ruby", "Ruby On Rails", "MapBox API", "Elastic Search", "Postgres", "Figma", "Heroku", "SQL", "Git", "StimulusJS", "Faker", "Cloudinary", "TypedJS", "jQuery", "PopperJS", "PlacesJS", "GeoCoder"],
       accordionTheme: "#FF5964",
       accordionButtonText: "GO TO MY POSH",
       accordionButtonURL: "https://my-posh-wedding-rental.herokuapp.com/"
