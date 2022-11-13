9589d245a6552ed0884d1cf8d1f22361dd74e6b5
Houzair Koussa <houzairmk@icloud.com>
2022-11-13 14:06:31 +0400

    [chore] cleanup

--- src/components/accordion-alt.tsx
+++ src/components/accordion-alt.tsx
@@ -27,7 +27,7 @@ const AccordionAlt: React.FunctionComponent<AccorionAltPropsInterface> = ({
     return (() => {
       window.removeEventListener("resize", ChangeAccordionAltLength);
     });
-  }, [])
+  }, []);
 
   useEffect(() => {
     ChangeAccordionAltLength();
@@ -59,11 +59,11 @@ const AccordionAlt: React.FunctionComponent<AccorionAltPropsInterface> = ({
     return result;
   }
 
-  const MoveUpToProject = () => {
+  const MoveToNextProject = () => {
     setCurrentProjectIndex(currentProjectIndex + 1);
   }
 
-  const MoveDownToProject = () => {
+  const MoveToPreviousProject = () => {
     setCurrentProjectIndex(currentProjectIndex - 1);
   }
 
@@ -72,26 +72,54 @@ const AccordionAlt: React.FunctionComponent<AccorionAltPropsInterface> = ({
       className="accordion-alt"
       accordionTheme={accordionsData[currentProjectIndex].accordionTheme || ''}
       isLightGlobalTheme={isLightGlobalTheme}
-      accordionAltLength={
-        accordionAltLength
-      }
+      accordionAltLength={accordionAltLength}
     >
       <div className="accordion-alt-buttons-container">
-        <button className="accordion-alt-button" disabled={currentProjectIndex <= 0} onClick={MoveDownToProject}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height={25} width={25} aria-hidden={true} className="accordion-alt-button-arrow">
+        <button 
+          className="accordion-alt-button" 
+          disabled={currentProjectIndex <= 0} 
+          onClick={MoveToPreviousProject}
+        >
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 448 512" 
+            height={25} 
+            width={25} 
+            aria-hidden={true} 
+            className="accordion-alt-button-arrow"
+          >
             <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
           </svg>
         </button>
-        <button className="accordion-alt-button" disabled={currentProjectIndex >= accordionsData?.length - 1} onClick={MoveUpToProject}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height={25} width={25} aria-hidden={true} className="accordion-alt-button-arrow">
-          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
-        </svg>
+        <button 
+          className="accordion-alt-button" 
+          disabled={currentProjectIndex >= accordionsData?.length - 1} 
+          onClick={MoveToNextProject}
+        >
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 448 512" 
+            height={25} 
+            width={25} 
+            aria-hidden={true} 
+            className="accordion-alt-button-arrow"
+          >
+            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+          </svg>
         </button>
       </div>
       <div className="accordion-alt-content-container">
         <div className="accordion-alt-content-main">
-          <h3 className="accordion-alt-content-main_title" ref={titleRef}>{accordionsData && accordionsData[currentProjectIndex]?.accordionTitle || ''}</h3>
-          <p className="accordion-alt-content-main_tech" ref={techTextRef}>
+          <h3 
+            className="accordion-alt-content-main_title" 
+            ref={titleRef}
+          >
+            {accordionsData && accordionsData[currentProjectIndex]?.accordionTitle || ''}
+          </h3>
+          <p 
+            className="accordion-alt-content-main_tech" 
+            ref={techTextRef}
+          >
             {accordionsData && accordionsData[currentProjectIndex]?.accordionTechParagraph || ''}
           </p>
           { !!accordionsData[currentProjectIndex]?.accordionButtonURL && (
@@ -120,7 +148,7 @@ const AccordionAltStyleWrapper = styled.div<{ theme: ThemeInterface, accordionTh
     border: 3px solid ${({ theme }) => theme.outline};
     color: ${({ theme }) => theme.text};
     padding: 0;
-    height: ${({ accordionAltLength }) => `${accordionAltLength + 170}px`};
+    height: ${({ accordionAltLength }) => `${accordionAltLength + 120}px`};
     transition: height 0.5s;
 
     .accordion-alt-buttons-container {
